3ae0d75194173f257994adf1c567681d13a77917
RishiBose961 <rishibose1901@gmail.com>
2024-12-02 20:24:33 +0530

    pwa notification

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,21 @@
-import { Navbar, NavbarBrand } from "@nextui-org/react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem} from "@nextui-org/react";
 
-export default function Header() {
+
+export default function App() {
   return (
     <Navbar>
       <NavbarBrand>
         <p className="font-bold text-inherit text-xl uppercase">Image Resize</p>
       </NavbarBrand>
+ 
+      <NavbarContent justify="end">
+        <NavbarItem className="flex">
+          <a href="https://github.com/RishiBose961">
+          <img src="/icons8-github.gif" loading="lazy" alt="loading" className="w-10 h-10 cursor-pointer"/>
+          </a>
+         
+        </NavbarItem>
+      </NavbarContent>
     </Navbar>
   );
 }
