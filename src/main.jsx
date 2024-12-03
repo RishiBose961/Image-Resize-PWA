86a17eaee39a19598749561d9e76eb4f2cecedc2
RishiBose961 <rishibose1901@gmail.com>
2024-12-03 09:23:34 +0530

    pwa notification work

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App.jsx";
 import { NextUIProvider } from "@nextui-org/react";
 
+
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <NextUIProvider>
